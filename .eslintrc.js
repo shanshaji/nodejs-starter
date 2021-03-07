1ee349b1f3e3d91e77f076d6cb1eb158db98e892
SHAN SHAJI <shanshaji@gmail.com>
2021-03-07 15:37:50 -0500

    Setting Up Lint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }]
+  }
+};
